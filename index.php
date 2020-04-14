f1a1de5f6a54e18cf6ac4c213fe7025a35d46d58
AgutuSam <sammyhardcell@gmail.com>
2020-04-14 09:00:25 +0300

    First commit Message

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from brandio.io/envato/iofrm/html/login2.html 
+by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 28 Aug 2019 06:17:32 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="shortcut icon" href="http://localhost/eproc/image/ddep_logo.png" style="height:1024px;"> -->
+    <title>DDep</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-theme2.css">
+</head>
+<body>
+    <div class="form-body">
+        <div class="website-logo">
+            <a href="index.html">
+                <div class="logo">
+                    <img src="../image/ddep_logo.png" alt="" style="width:130px;">
+                </div>
+            </a>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder">
+
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3> The novelty COVID-19 infections estimator.</h3>
+                        <p></p>
+                        <!-- <p>Access to an efficient electoral process for this Digital Democratic Age.</p> -->
+                        <!-- <div class="page-links">
+                            <a href="login2.html" class="active">Login</a><a href="register2.html">Register</a>
+                        </div> -->
+    <form action="login.php" method="GET">
+    <input class="form-control" type="text" name="data-population" required placeholder="Population" id="focusedInput">
+    <input class="form-control" type="text" name="data-time-to-elapse" required placeholder="Time To Elapse" id="focusedInput">
+    <input class="form-control" type="text" name="data-reported-cases" required placeholder="Reported Cases" id="focusedInput">
+    <input class="form-control" type="text" name="data-total-hospital-beds" required placeholder="Total Hospital Beds" id="focusedInput">
+    <select class="sel form-control" id="focusedInput" 
+    
+    >
+    <option>days</option>
+    <option>weeks</option>
+    <option>months</option>
+    </select>
+    <!-- <input type="checkbox" id="chk1"><label for="chk1">Remmeber me</label> -->
+    <div class="form-button">
+    <button id="submit" type="submit" class="ibtn"  name="data-goestimate" id="bttn" ><a href="#"></a> Estimate</button> 
+                                <!-- <a href="forget2.html">Forget password?</a> -->
+                            </div>
+                        </form>
+                        <div class="other-links">
+                            <span>Research - Advocacy - Advisory</span>
+                            <!-- <a href="#">Facebook</a><a href="#">Google</a><a href="#">Linkedin</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- <script src="../e-js/jquery-3.2.1.min.js"></script>
+<script src="../e-js/popper.min.js"></script>
+<script src="../e-js/bootstrap.min.js"></script>
+<script src="../e-js/main.js"></script> -->
+</body>
+
+<!-- Mirrored from brandio.io/envato/iofrm/html/login2.html
+ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 28 Aug 2019 06:17:35 GMT -->
+</html>
